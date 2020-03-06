fc83aaaa7738e2112c38feaabf356911aaddd520
shuhul <shuhulmujoo@gmail.com>
2020-03-05 20:41:03 -0800

    Auton Final V1

--- TeamCode/src/main/java/auto/AutoBlue.java
+++ TeamCode/src/main/java/auto/AutoBlue.java
@@ -3,6 +3,7 @@ package auto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 import autoFunctions.Path;
@@ -10,63 +11,133 @@ import autoFunctions.RobotFunctions;
 import autoFunctions.TerraCV;
 import global.TerraBot;
 import util.CodeSeg;
+import util.Rect;
 
 @Autonomous(name = "AutoBlue", group = "Auto")
 public class AutoBlue extends LinearOpMode {
     TerraBot bot = new TerraBot();
     RobotFunctions rf = new RobotFunctions();
     TerraCV cv = new TerraCV();
-    Path toStone = new Path();
-    Path toFoundation = new Path();
+    Path path = new Path();
+    Path path2 = new Path();
+    Path path3 = new Path();
+    Path path4 = new Path();
 
     @Override
     public void runOpMode() {
         initialize();
-        rf.telemetryText("ready");
+        rf.telemetryText("done initing");
+        rf.generateRandomIM();
+        rf.pauseBeforeInit(4);
+        rf.scanStonesBeforeInit(cv);
+        //rf.startOdoThread();
         waitForStart();
-//        toStone.addPose(10,0,0);
-//        rf.scanStones(toStone,cv);
-//        toStone.addPose(0,0,0);
-//        rf.start(toStone,this);
-//        toFoundation.continuePath(toStone);
-//        if(rf.stonePos.equals(TerraCV.StonePos.RIGHT)){
-//            toFoundation.addPose(23,1,-40);
-//            rf.intake(toFoundation, 1);
-//            toFoundation.addPose(2,4,-10);
-//            toFoundation.addPose(-14,-14,133);
-//        }else if(rf.stonePos.equals(TerraCV.StonePos.MIDDLE)){
-//            toFoundation.addPose(23,10,40);
-//            rf.intake(toFoundation, 1);
-//            toFoundation.addPose(2,-4,10);
-//            toFoundation.addPose(-12,-17,32);
-//        }else {
-//            toFoundation.addPose(23,2,40);
-//            rf.intake(toFoundation, 1);
-//            toFoundation.addPose(2,-4,10);
-//            toFoundation.addPose(-12,-11,32);
-//        }
-//        grabStone();
-//        toFoundation.addPose(0,-50,0);
-//        rf.flip(toFoundation, 0.9, 0.8);
-//        toFoundation.addPose(4,-20,85);
-//        rf.grab(toFoundation, bot.sp2);
-//        rf.pause(toFoundation, 500);
-//        rf.flip(toFoundation, bot.sp, bot.sp);
-//
-//
-//        rf.setScale(toFoundation,1.5, true);
-//        toFoundation.addPose(4,0,0);
-//        rf.grabFoundation(toFoundation, 1.2);
-//        rf.pause(toFoundation, 1000);
-//        rf.setScale(toFoundation, 4, true);
-//        toFoundation.addPose(-34.5,-1,0);
-//        rf.setScale(toFoundation, 1.05, false);
-//        rf.grabFoundation(toFoundation, 0);
-//        rf.pause(toFoundation, 1000);
-//        toFoundation.addPose(0,30,0);
-//        toFoundation.addPose(25, 0, 0);
-//        toFoundation.addPose(0,20,0);
-//        rf.start(toFoundation,this);
+        if(rf.stonePos.equals(TerraCV.StonePos.RIGHT)){
+            path.addPose(34,0,-55, true);
+            rf.intake(path, 1);
+            path.addPose(3,5,-10, false);
+            path.addPose(-15, -20, -25,false);
+        }else if(rf.stonePos.equals(TerraCV.StonePos.MIDDLE)){
+            path.addPose(34,-8,-55, true);
+            rf.intake(path, 1);
+            path.addPose(3,5,-10, false);
+            path.addPose(-15, -12, -25,false);
+        }else {
+            path.addPose(34,2,55, true);
+            rf.intake(path, 1);
+            path.addPose(3,-5,10, false);
+            path.addPose(-12, -12, -155,false);
+        }
+        grabStone(path);
+        path.addPose(-2,-40,0, false);
+        rf.flip(path, 0.8, 0.8);
+        rf.grabFoundation(path, 0.6);
+        rf.multiplyD(path, 2.5);
+        rf.setAccuracy(path, 0.5, 0.5, 2);
+        path.addPose(0,-30,-90, true);
+        rf.setAccuracy(path, 1, 1, 6);
+        rf.multiplyD(path, 2);
+        path.addPose(6, 0, 0, false);
+        rf.grabFoundation(path, 0.85);
+        dropStone(path);
+        rf.setScale(path, 7);
+        path.addPose(-10,20, 50, false);
+        path.addPose(5, -8, 40, false);
+        rf.grabFoundation(path, 0);
+        rf.start(path, this);
+        path2.continuePath(path);
+
+        if(rf.stonePos.equals(TerraCV.StonePos.RIGHT)){
+            path2.addPose(4, 96, 0, true);
+            rf.intake(path2, 1);
+            path2.addPose(11,1,30, true);
+            path2.addPose(2,4,-10,false);
+            path2.addPose(-16, -20, -20, false);
+            grabStone(path2);
+            path2.addPose(-2,-40,0,false);
+        }else if(rf.stonePos.equals(TerraCV.StonePos.MIDDLE)){
+            path2.addPose(4, 86, 0, true);
+            rf.intake(path2, 1);
+            path2.addPose(9,1,30, true);
+            path2.addPose(2,6,-10,false);
+            path2.addPose(-15, -17, -20, false);
+            grabStone(path2);
+            path2.addPose(-2,-37,0,false);
+        }else {
+            path2.addPose(4, 77, 0, true);
+            rf.intake(path2, 1);
+            path2.addPose(12,1,30, true);
+            path2.addPose(2,5,-10,false);
+            path2.addPose(-16, -2, -20, false);
+            grabStone(path2);
+            path2.addPose(-2,-38,0,false);
+        }
+        rf.flip(path2, 0.8, 0.8);
+        path2.addPose(-4,-43,0, true);
+        dropStone(path2);
+        rf.start(path2, this);
+        bot.move(0,0,0);
+        path3.continuePath(path2);
+        rf.pause(path3, 200);
+        path3.addPose(4,43,0, false);
+        if(rf.stonePos.equals(TerraCV.StonePos.RIGHT)){
+            path3.addPose(4, 20, 0, false);
+            path3.addPose(20,23,20, true);
+            rf.intake(path3, 1);
+            rf.setScale(path3, 0.5);
+            path3.addPose(5, 8, 0, false);
+            rf.setScale(path3, 1);
+            path3.addPose(-29, -10, -20, false);
+            grabStone(path3);
+            path3.addPose(0,-40,0, false);
+        }else if(rf.stonePos.equals(TerraCV.StonePos.MIDDLE)){
+            path3.addPose(4, 54, 0, true);
+            rf.intake(path3, 1);
+            path3.addPose(8,1,30, true);
+            path3.addPose(2,4,-10,false);
+            path3.addPose(-13, -20, -20, false);
+            grabStone(path3);
+            path3.addPose(0,-40,0,false);
+        }else {
+            path3.addPose(4, 40, 0, true);
+            rf.intake(path3, 1);
+            path3.addPose(6,1,30, true);
+            path3.addPose(2,6,-10,false);
+            path3.addPose(-16, -20, -20, false);
+            grabStone(path3);
+            path3.addPose(0,-32,0,false);
+        }
+        rf.flip(path3, 0.8, 0.8);
+        path3.addPose(0,-43,0, true);
+        rf.start(path3, this);
+        bot.move(0,0,0);
+        path4.continuePath(path3);
+        dropStone(path4);
+        rf.pause(path4, 400);
+        path4.addPose(0,40, 0, true);
+        rf.start(path4, this);
+        bot.move(0,0,0);
+       //rf.odoThread.stop();
 
     }
 
@@ -76,12 +147,31 @@ public class AutoBlue extends LinearOpMode {
         cv.init(bot,this, 3);
     }
 
-    private void grabStone(){
-        rf.flip(toFoundation, 0,0);
-        rf.pause(toFoundation,100);
-        rf.grab(toFoundation,1);
-        rf.intake(toFoundation,0);
+    private void grabStone(Path p){
+        CodeSeg grabStone = new CodeSeg() {
+            @Override
+            public void run() {
+                ElapsedTime timer = new ElapsedTime();
+                bot.flip(0,0);
+                while (timer.seconds() < 0.6){}
+                bot.grab(1);
+                bot.intake(0);
+            }
+        };
+        rf.customThread(p, grabStone);
     }
 
+    private void dropStone(Path p){
+        CodeSeg dropStone = new CodeSeg() {
+            @Override
+            public void run() {
+                ElapsedTime timer = new ElapsedTime();
+                bot.grab(bot.sp2);
+                while (timer.seconds() < 0.5){}
+                bot.flip(bot.sp, bot.sp);
+            }
+        };
+        rf.customThread(p, dropStone);
+    }
 
 }
